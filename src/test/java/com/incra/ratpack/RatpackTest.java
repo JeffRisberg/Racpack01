57b650f9b2a2c00f894d569f0f45f845aa931a46
JeffRisberg <jrisberg@aol.com>
2016-06-03 11:43:35 -0700

    updated tests

--- src/test/java/com/incra/ratpack/RatpackTest.java
+++ src/test/java/com/incra/ratpack/RatpackTest.java
@@ -26,27 +26,6 @@ public class RatpackTest {
     public void redirectsToIndexHtml() {
         final ReceivedResponse response = httpClient.get();
         assertEquals(200, response.getStatusCode());
-        assertThat(response.getBody().getText(), containsString("<h1>index</h1>"));
-    }
-
-    @Test
-    public void requiresFormAuth() {
-        ReceivedResponse response = httpClient.get("form/index.html");
-        assertEquals(200, response.getStatusCode());
-        assertThat(response.getBody().getText(), containsString("<form action=\"" + aut.getAddress() + "callback?client_name=FormClient\" method=\"POST\">"));
-
-        response = httpClient
-            .requestSpec(r -> r
-                    .body(b -> b.text("username=foo&password=foo"))
-                    .headers(h -> h
-                            .set(HttpHeaderNames.CONTENT_TYPE, "application/x-www-form-urlencoded")
-                            .set(HttpHeaderNames.REFERER, aut.getAddress())
-                    )
-            )
-            .params(m -> m.put("client_name", "FormClient"))
-            .post("callback");
-
-        assertThat(response.getBody().getText(), containsString("attributes: {username=foo}"));
-        assertEquals(200, response.getStatusCode());
+        assertThat(response.getBody().getText(), containsString("root handler!"));
     }
 }
