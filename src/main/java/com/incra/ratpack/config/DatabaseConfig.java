6345ebb15c115083a6cd8ecc5c19c955a817b857
JeffRisberg <jrisberg@aol.com>
2017-05-03 23:32:37 -0700

    added persistance unit name to db config

--- src/main/java/com/incra/ratpack/config/DatabaseConfig.java
+++ src/main/java/com/incra/ratpack/config/DatabaseConfig.java
@@ -5,21 +5,30 @@ package com.incra.ratpack.config;
  * @since 12/01/16
  */
 public class DatabaseConfig {
+    protected Boolean encrypted = false;
     protected String server = "localhost";
     protected Integer portNumber = 3000;
     protected String databaseName = "myDB";
     protected String username = "root";
     protected String password;
+    protected String persistanceUnitName = "ratpack-jpa";
 
     public DatabaseConfig() {
     }
 
-    public DatabaseConfig(String server, Integer portNumber, String databaseName, String username, String password) {
+    public DatabaseConfig(Boolean encrypted, String server, Integer portNumber,
+                          String databaseName, String username, String password, String persistanceUnitName) {
+        this.encrypted = encrypted;
         this.server = server;
         this.portNumber = portNumber;
         this.databaseName = databaseName;
         this.username = username;
         this.password = password;
+        this.persistanceUnitName = persistanceUnitName;
+    }
+
+    public Boolean getEncrypted() {
+        return encrypted;
     }
 
     public String getServer() {
@@ -40,6 +49,10 @@ public class DatabaseConfig {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getPersistanceUnitName() {
+        return persistanceUnitName;
     }
 
     public String toString() {
