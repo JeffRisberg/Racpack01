be1749fd3ee08729a91501f2858deb09c5b3f71d
JeffRisberg <jrisberg@aol.com>
2017-05-23 23:06:20 -0700

    added example of using Session

--- src/main/java/com/incra/ratpack/Ratpack01.java
+++ src/main/java/com/incra/ratpack/Ratpack01.java
@@ -10,6 +10,8 @@ import ratpack.guice.Guice;
 import ratpack.handlebars.HandlebarsModule;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
+import ratpack.session.Session;
+import ratpack.session.SessionModule;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -38,38 +40,55 @@ public class Ratpack01 {
         m.put("campaigns", campaigns);
 
         RatpackServer.start(s -> s
-                        .serverConfig(ctx -> {
-                                    ctx.baseDir(BaseDir.find());
-                                    ctx.json("databaseConfig.json");
-                                    ctx.require("/database", DatabaseConfig.class);
-                                }
-                        )
-                        .registry(Guice.registry(b -> {
-                            b.module(CampaignModule.class);
-                            b.module(HandlebarsModule.class);
-                        }))
-                        .handlers(chain -> chain
-                                        .path("foo", ctx -> ctx.render("from the foo handler")) // Map to /foo
-                                        .path("bar", ctx -> ctx.render("from the bar handler")) // Map to /bar
-                                        .prefix("nested", nested -> { // Set up a nested routing block, which is delegated to `nestedHandler`
-                                            nested.path(":var1/:var2?", ctx -> { // The path tokens are the :var1 and :var2 path components above
-                                                Map<String, String> pathTokens = ctx.getPathTokens();
-                                                ctx.render(
-                                                        "from the nested handler, " +
-                                                                " var1: " + pathTokens.get("var1") +
-                                                                ", var2: " + pathTokens.get("var2")
-                                                );
-                                            });
-                                        })
-                                        .path("x", ctx -> ctx.render(ctx.get(DatabaseConfig.class).toString()))
-                                        .prefix("static", n -> n.files(files -> files.dir("static")))
-                                        .path("injected", CampaignHandler.class) // Map to a dependency injected handler
-                                                //.prefix("static", n -> n.fileSystem("static", Chain::files)) // Bind the /static app path to the src/ratpack/assets/images dir
-                                        .path("handlebars", ctx ->
-                                                ctx.render(handlebarsTemplate("template.html", m, "text/html")))
-                                        .path("json", ctx -> ctx.render(json(formDataResponse(m))))
-                                        .all(ctx -> ctx.render("root handler!"))
-                        )
+                .serverConfig(ctx -> {
+                            ctx.baseDir(BaseDir.find());
+                            ctx.json("databaseConfig.json");
+                            ctx.require("/database", DatabaseConfig.class);
+                        }
+                )
+                .registry(Guice.registry(b -> {
+                    b.module(CampaignModule.class);
+                    b.module(HandlebarsModule.class);
+                    b.module(SessionModule.class);
+                }))
+                .handlers(chain -> chain
+                        .all(ctx -> {
+                            System.out.println("initial handler!");
+                            ctx.get(Session.class).getData().then(sessionData -> {
+                                Integer count = ((Optional<Integer>) sessionData.get("req-count"))
+                                        .orElse(new Integer(0));
+                                count = count + 1;
+                                sessionData.set("req-count", count);
+                                ctx.next();
+                            });
+                        })
+                        .path("count", ctx -> {
+                            ctx.get(Session.class).get("req-count").then(optCount -> {
+                                Integer count = ((Optional<Integer>) optCount).get();
+                                ctx.render("count=" + count);
+                            });
+                        })
+                        .path("foo", ctx -> ctx.render("from the foo handler")) // Map to /foo
+                        .path("bar", ctx -> ctx.render("from the bar handler")) // Map to /bar
+                        .prefix("nested", nested -> { // Set up a nested routing block, which is delegated to `nestedHandler`
+                            nested.path(":var1/:var2?", ctx -> { // The path tokens are the :var1 and :var2 path components above
+                                Map<String, String> pathTokens = ctx.getPathTokens();
+                                ctx.render(
+                                        "from the nested handler, " +
+                                                " var1: " + pathTokens.get("var1") +
+                                                ", var2: " + pathTokens.get("var2")
+                                );
+                            });
+                        })
+                        .path("x", ctx -> ctx.render(ctx.get(DatabaseConfig.class).toString()))
+                        .prefix("static", n -> n.files(files -> files.dir("static")))
+                        .path("injected", CampaignHandler.class) // Map to a dependency injected handler
+                        //.prefix("static", n -> n.fileSystem("static", Chain::files)) // Bind the /static app path to the src/ratpack/assets/images dir
+                        .path("handlebars", ctx ->
+                                ctx.render(handlebarsTemplate("template.html", m, "text/html")))
+                        .path("json", ctx -> ctx.render(json(formDataResponse(m))))
+                        .all(ctx -> ctx.render("root handler!"))
+                )
         );
     }
 
